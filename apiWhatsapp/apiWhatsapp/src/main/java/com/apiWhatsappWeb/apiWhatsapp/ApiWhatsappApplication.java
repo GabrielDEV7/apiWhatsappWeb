18eeadcb30ee526d2d1ed76736c583eaa710847c
GabrielDEV7 <135471334+GabrielDEV7@users.noreply.github.com>
2024-04-21 14:22:00 -0300

    api

---
+++ apiWhatsapp/apiWhatsapp/src/main/java/com/apiWhatsappWeb/apiWhatsapp/ApiWhatsappApplication.java
@@ -0,0 +1,13 @@
+package com.apiWhatsappWeb.apiWhatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWhatsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWhatsappApplication.class, args);
+	}
+
+}
---
+++ apiWhatsapp/apiWhatsapp/src/test/java/com/apiWhatsappWeb/apiWhatsapp/ApiWhatsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiWhatsappWeb.apiWhatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWhatsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
